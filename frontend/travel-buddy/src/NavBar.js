3bf06d3ee642cd18df64662aad6408accf566815
riajames <riajames.17@gmail.com>
2024-02-24 12:49:11 -0600

    set up router and files

---
+++ frontend/travel-buddy/src/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from "./assets/tbLogo.png";
+
+function NavBar() {
+    return (
+        <div className="navbar-container">
+            <h1>
+                <img src={logo} alt={"Logo"} width={50} height={50}/>
+                NavBar
+            </h1>
+
+        </div>
+    );
+}
+
+export default NavBar;
